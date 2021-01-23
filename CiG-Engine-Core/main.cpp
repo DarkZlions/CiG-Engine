639375058661cd3c3ceca483b10aae390ef2c609
DarkLions <darklions888@gmail.com>
2021-01-23 20:29:07 +0100

    Setup Window

--- CiG-Engine-Core/main.cpp
+++ CiG-Engine-Core/main.cpp
@@ -2,16 +2,25 @@
 #include <iostream>
 
 #include "src/misc/errorCallback.h"
+#include "src/graphics/window.h"
 
 int main(void)
 {
 	glfwSetErrorCallback(error_callback);
 
-	// Kommentar von Leon
+	using namespace cig;
+	using namespace graphics;
 
 	if (!glfwInit()) 
 	{
 		exit(EXIT_FAILURE);
+	}
+
+	Window window("CiG Engine", 640, 480);
+
+	while (!window.closed())
+	{
+		window.update();
 	}
 
 	system("PAUSE");
